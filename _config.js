0c16ce3351f6a8c354e6981c03c3c52654992b53
Mary-softeng <marysofteng@gmail.com>
2024-04-12 17:56:36 +0300

    Updated connection strings on config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://marynjuguna1:gPGUc5LbpSoig3D1@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://marynjuguna1:gPGUc5LbpSoig3D1@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://marynjuguna1:gPGUc5LbpSoig3D1@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Amos_test:Today123@cluster0.5g1v39y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://Amos_test:Today123@cluster0.5g1v39y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://Amos_test:Today123@cluster0.5g1v39y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    
+    
 }
 module.exports = config;
